52bfaabc74518dcbf772923f5c13d17c0463b035
OchreFox <contact@ochrefox.net>
2022-07-23 17:54:19 -0500

    Fixed build bugs

--- components/StandardItem.tsx
+++ components/StandardItem.tsx
@@ -2,10 +2,9 @@ import { css, cx } from '@emotion/css'
 import { motion } from 'framer-motion'
 import _ from 'lodash'
 import React, { useContext, useEffect, useRef, useState } from 'react'
-import ReactDOM from 'react-dom'
+import { createPortal } from 'react-dom'
 import JsxParser from 'react-jsx-parser'
 import { usePopper } from 'react-popper'
-import { Portal } from 'react-portal'
 
 // SVG imports
 import goldIcon from '../public/icons/gold.svg?url'
@@ -116,103 +115,102 @@ export const StandardItem = ({
   }
 
   return (
-    <>
-      <div className="relative" ref={addToRefs}>
-        <motion.div
-          layout
-          transition={transition}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          key={item.id}
-          className={cx(
-            'group -m-1 flex cursor-pointer flex-col items-center px-2 py-2 text-center',
-            css`
-              border: 2px solid rgba(0, 0, 0, 0);
-            `,
-            selectedItem !== null &&
-              selectedItem.id === item.id &&
-              cx(
-                css`
-                  &:before {
-                    content: '';
-                    position: absolute;
-                    width: 100%;
-                    height: 100%;
-                    top: 0;
-                    left: 0;
-                    z-index: -1;
-                    background: linear-gradient(
-                      180deg,
-                      rgba(0, 0, 0, 0) 0%,
-                      rgba(204, 41, 54, 0.75) 50%,
-                      rgba(0, 0, 0, 0) 100%
-                    );
-                    background-size: 600% 600%;
+    <div className="relative" ref={addToRefs}>
+      <motion.div
+        layout
+        transition={transition}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        key={item.id}
+        className={cx(
+          'group -m-1 flex cursor-pointer flex-col items-center px-2 py-2 text-center',
+          css`
+            border: 2px solid rgba(0, 0, 0, 0);
+          `,
+          selectedItem !== null &&
+            selectedItem.id === item.id &&
+            cx(
+              css`
+                &:before {
+                  content: '';
+                  position: absolute;
+                  width: 100%;
+                  height: 100%;
+                  top: 0;
+                  left: 0;
+                  z-index: -1;
+                  background: linear-gradient(
+                    180deg,
+                    rgba(0, 0, 0, 0) 0%,
+                    rgba(204, 41, 54, 0.75) 50%,
+                    rgba(0, 0, 0, 0) 100%
+                  );
+                  background-size: 600% 600%;
 
-                    animation: ${usePotatoMode('scroll 10s linear infinite;', CSSProperty.ANIMATION)} @keyframes scroll {
-                      0% {
-                        background-position: 50% 0%;
-                      }
-                      100% {
-                        background-position: 50% -600%;
-                      }
+                  animation: ${usePotatoMode('scroll 10s linear infinite;', CSSProperty.ANIMATION)} @keyframes scroll {
+                    0% {
+                      background-position: 50% 0%;
+                    }
+                    100% {
+                      background-position: 50% -600%;
                     }
                   }
+                }
 
-                  border: 2px solid;
-                  box-sizing: content-box;
-                  @supports (background: paint(something)) {
-                    border-image: linear-gradient(var(--angle), #12c2e9, #b9f5ff, #c471ed, #14fff5, #f64f59) 1;
-                    animation: ${usePotatoMode('6s rotate linear infinite forwards;', CSSProperty.ANIMATION)} @keyframes
-                      rotate {
-                      to {
-                        --angle: 360deg;
-                      }
-                    }
-                    @property --angle {
-                      syntax: '<angle>';
-                      initial-value: 0deg;
-                      inherits: false;
-                    }
-                  }
-                  /* Rotate the gradient */
-                  --angle: 0deg;
+                border: 2px solid;
+                box-sizing: content-box;
+                @supports (background: paint(something)) {
                   border-image: linear-gradient(var(--angle), #12c2e9, #b9f5ff, #c471ed, #14fff5, #f64f59) 1;
                   animation: ${usePotatoMode('6s rotate linear infinite forwards;', CSSProperty.ANIMATION)} @keyframes
                     rotate {
-                    from {
-                      --angle: 0deg;
-                    }
                     to {
                       --angle: 360deg;
                     }
                   }
-                `
-              )
-          )}
-          ref={buttonRef}
-          onMouseEnter={() => {
-            setMouseEnter(true)
-          }}
-          onMouseLeave={() => {
-            setMouseEnter(false)
-          }}
-          onClick={() => {
-            // Toggle selectedItem
-            if (selectedItem !== null && selectedItem.id === item.id) {
-              setSelectedItem(null)
-            } else {
-              setSelectedItem(item)
-            }
-          }}
-        >
-          <div
-            className={
-              isMythic
-                ? cx(
-                    'relative inline-flex shrink-0 items-center justify-center border border-yellow-700',
-                    css`
+                  @property --angle {
+                    syntax: '<angle>';
+                    initial-value: 0deg;
+                    inherits: false;
+                  }
+                }
+                /* Rotate the gradient */
+                --angle: 0deg;
+                border-image: linear-gradient(var(--angle), #12c2e9, #b9f5ff, #c471ed, #14fff5, #f64f59) 1;
+                animation: ${usePotatoMode('6s rotate linear infinite forwards;', CSSProperty.ANIMATION)} @keyframes
+                  rotate {
+                  from {
+                    --angle: 0deg;
+                  }
+                  to {
+                    --angle: 360deg;
+                  }
+                }
+              `
+            )
+        )}
+        ref={buttonRef}
+        onMouseEnter={() => {
+          setMouseEnter(true)
+        }}
+        onMouseLeave={() => {
+          setMouseEnter(false)
+        }}
+        onClick={() => {
+          // Toggle selectedItem
+          if (selectedItem !== null && selectedItem.id === item.id) {
+            setSelectedItem(null)
+          } else {
+            setSelectedItem(item)
+          }
+        }}
+      >
+        <div
+          className={
+            isMythic
+              ? cx(
+                  'relative inline-flex shrink-0 items-center justify-center border border-yellow-700',
+                  css`
                       background: linear-gradient(
                         180deg,
                         rgba(234, 179, 8, 1) 0%,
@@ -234,9 +232,9 @@ export const StandardItem = ({
                         }
                       }
                     `,
-                    hoveredItem !== null &&
-                      item.id !== hoveredItem &&
-                      css`
+                  hoveredItem !== null &&
+                    item.id !== hoveredItem &&
+                    css`
                         opacity: ${usePotatoMode('0.5;', CSSProperty.OPACITY)}
                         transition-property: ${usePotatoMode(
                           'color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter;',
@@ -244,51 +242,51 @@ export const StandardItem = ({
                         )};
                         transition-duration: 100ms;
                       `
-                  )
-                : ''
-            }
-          >
-            {/* Display the item icon */}
-            <img
-              src={item.icon ?? ''}
-              alt={item.name ?? ''}
-              className={cx(
-                'border border-black object-cover ring-1 ring-yellow-700 duration-100 group-hover:z-30 group-hover:ring-2 group-hover:brightness-125',
-                state.enabled ? 'transition-none' : 'transition',
-                !isMythic &&
-                  hoveredItem !== null &&
-                  item.id !== hoveredItem &&
-                  css`
+                )
+              : ''
+          }
+        >
+          {/* Display the item icon */}
+          <img
+            src={item.icon ?? ''}
+            alt={item.name ?? ''}
+            className={cx(
+              'border border-black object-cover ring-1 ring-yellow-700 duration-100 group-hover:z-30 group-hover:ring-2 group-hover:brightness-125',
+              state.enabled ? 'transition-none' : 'transition',
+              !isMythic &&
+                hoveredItem !== null &&
+                item.id !== hoveredItem &&
+                css`
                     opacity: ${usePotatoMode('0.5;', CSSProperty.OPACITY)}
                     transition-property: ${usePotatoMode(
                       'color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter;',
                       CSSProperty.TRANSITION_PROPERTY
                     )};
                   `
-              )}
-              width={50}
-              height={50}
-            />
-          </div>
-          <p
-            className={cx(
-              'font-sans text-gray-200 group-hover:text-yellow-200',
-              hoveredItem !== null &&
-                item.id !== hoveredItem &&
-                css`
+            )}
+            width={50}
+            height={50}
+          />
+        </div>
+        <p
+          className={cx(
+            'font-sans text-gray-200 group-hover:text-yellow-200',
+            hoveredItem !== null &&
+              item.id !== hoveredItem &&
+              css`
                   opacity: ${usePotatoMode('0.5;', CSSProperty.OPACITY)}
                   transition-property: ${usePotatoMode(
                     'color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter;',
                     CSSProperty.TRANSITION_PROPERTY
                   )};
                 `
-            )}
-          >
-            {item.gold?.total}
-          </p>
-        </motion.div>
-        {showPopper && (
-          <Portal node={document && document.querySelector('#item-container')}>
+          )}
+        >
+          {item.gold?.total}
+        </p>
+      </motion.div>
+      {showPopper
+        ? createPortal(
             <Tooltip
               ref={popperRef}
               style={styles.popper}
@@ -345,10 +343,10 @@ export const StandardItem = ({
                   )}
                 </div>
               </div>
-            </Tooltip>
-          </Portal>
-        )}
-      </div>
-    </>
+            </Tooltip>,
+            document.querySelector('#item-container') as HTMLElement
+          )
+        : null}
+    </div>
   )
 }
