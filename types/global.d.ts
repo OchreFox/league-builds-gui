1e2656377d52bd2b350048e1dbe379bc5034f659
OchreFox <contact@ochrefox.net>
2022-11-28 19:32:22 -0600

    Updated dependencies

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,14 +1,14 @@
-import 'little-state-machine'
+// import 'little-state-machine'
 
-import Block from './Build'
+// import Block from './Build'
 
-declare module 'little-state-machine' {
-  interface GlobalState {
-    itemBuild: {
-      title: string
-      associatedMaps: number[] // SR -> 11, ARAM -> 12
-      associatedChampions: number[]
-      blocks: Block[]
-    }
-  }
-}
+// declare module 'little-state-machine' {
+//   interface GlobalState {
+//     itemBuild: {
+//       title: string
+//       associatedMaps: number[] // SR -> 11, ARAM -> 12
+//       associatedChampions: number[]
+//       blocks: Block[]
+//     }
+//   }
+// }
